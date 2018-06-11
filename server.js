43c5b542836f5f11b286326b0f092fb118608eac
hodaya55 <hodayah159@gmail.com>
2018-06-11 19:58:24 +0300

    update for heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-app.listen(4000);
+// app.listen(4000);
 
 app.use(express.static('public'));
 app.use(express.static('node_modules'));
@@ -8,3 +8,6 @@ app.use(express.static('node_modules'));
 app.get('/', function(request, response) {
   response.send('Hey, hello from the server!');
 });
+
+
+app.listen(process.env.PORT || '4000');
